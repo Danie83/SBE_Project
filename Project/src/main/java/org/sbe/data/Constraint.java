4c50990630b309c38cb720b4541f835eb57abc76
Filimon Dănuț-Dumitru <60850899+Danie83@users.noreply.github.com>
2023-06-07 22:09:38 +0300

    added method to evaluate constraints

--- Project/src/main/java/org/sbe/data/Constraint.java
+++ Project/src/main/java/org/sbe/data/Constraint.java
@@ -1,5 +1,11 @@
 package org.sbe.data;
 
+import java.lang.reflect.Field;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class Constraint
 {
     private String factor;
@@ -18,9 +24,9 @@ public class Constraint
         return factor;
     }
 
-    public void setFactor(String factor)
+    public void setFactor(String newFactor)
     {
-        this.factor = factor;
+        this.factor = newFactor.equals("station_id") ? "stationId" : newFactor;
     }
 
     public String getOperator()
@@ -41,5 +47,128 @@ public class Constraint
     public void setRequiredValue(String requiredValue)
     {
         this.requiredValue = requiredValue;
+    }
+
+    private boolean evaluateConstraint(Publication publication)
+    throws NoSuchFieldException, IllegalAccessException
+    {
+        Class<?> targetPublicationClass = publication.getClass();
+        Field field = targetPublicationClass.getDeclaredField(factor);
+        field.setAccessible(true);
+        Object value = field.get(publication);
+
+        switch (operator)
+        {
+            case "==":
+                return compareEqual(value, requiredValue);
+            case "!=":
+                return !compareEqual(value, requiredValue);
+            case "<":
+                return compareLower(value, requiredValue);
+            case ">=":
+                return !compareLower(value, requiredValue);
+            case ">":
+                return compareGreater(value, requiredValue);
+            case "<=":
+                return !compareGreater(value, requiredValue);
+            default:
+                break;
+        }
+    }
+
+    private boolean compareEqual(Object value, String requiredValue)
+    {
+        if (value instanceof Integer)
+        {
+            return Integer.parseInt(requiredValue) == (Integer) value;
+        }
+        else if (value instanceof Float)
+        {
+            return Float.parseFloat(requiredValue) == (Float) value;
+        }
+        else if (value instanceof Date)
+        {
+            Date dateValue = (Date) value;
+            DateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy");
+            try
+            {
+                Date requiredDate = dateFormat.parse(requiredValue);
+                return dateValue.equals(requiredDate);
+            }
+            catch (ParseException e)
+            {
+                e.printStackTrace();
+                return false;
+            }
+        }
+        else if (value instanceof String)
+        {
+            return requiredValue.equals(value);
+        }
+        return false;
+    }
+
+    private boolean compareLower(Object value, String requiredValue)
+    {
+        if (value instanceof Integer)
+        {
+            return Integer.parseInt(requiredValue) < (Integer) value;
+        }
+        else if (value instanceof Float)
+        {
+            return Float.parseFloat(requiredValue) < (Float) value;
+        }
+        else if (value instanceof Date)
+        {
+            Date dateValue = (Date) value;
+            DateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy");
+            try
+            {
+                Date requiredDate = dateFormat.parse(requiredValue);
+                return dateValue.compareTo(requiredDate) < 0;
+            }
+            catch (ParseException e)
+            {
+                e.printStackTrace();
+                return false;
+            }
+        }
+        else if (value instanceof String)
+        {
+            return requiredValue.equals(value);
+        }
+        return false;
+    }
+
+    private boolean compareGreater(Object value, String requiredValue)
+    {
+        if (value instanceof Integer)
+        {
+            return Integer.parseInt(requiredValue) > (Integer) value;
+        }
+        else if (value instanceof Float)
+        {
+            return Float.parseFloat(requiredValue) > (Float) value;
+        }
+        else if (value instanceof Date)
+        {
+            Date dateValue = (Date) value;
+            DateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy");
+            try
+            {
+                Date requiredDate = dateFormat.parse(requiredValue);
+                return dateValue.compareTo(requiredDate) > 0;
+            }
+            catch (ParseException e)
+            {
+                e.printStackTrace();
+                return false;
+            }
+        }
+        else if (value instanceof String)
+        {
+            return requiredValue.equals(value);
+        }
+        return false;
     }
 }
